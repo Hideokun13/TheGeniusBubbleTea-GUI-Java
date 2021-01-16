4e74c76d4aff1e39767832cf30738d6bc94e784b
Hideokun13 <boypeerawit@hotmail.com>
2021-01-16 13:27:40 +0700

    Added LoginPage and MainPage UI

---
+++ src/thegeniusbubbletea/gui/TheGeniusBubbleTeaGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thegeniusbubbletea.gui;
+
+/**
+ *
+ * @author Asus
+ */
+public class TheGeniusBubbleTeaGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginPage_UI login_ui = new LoginPage_UI();
+        login_ui.setVisible(true);
+    }
+    
+}
